5d6f3b87a2e8d67490926252f9dc029b14e9a0f8
MaxMEllon <maxmellon1994@gmail.com>
2016-09-22 05:38:21 +0900

    :wrench: Modify Swagger config

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -1,7 +1,8 @@
 Swagger::Docs::Config.register_apis({
   '1.0' => {
-    base_path: '/',
-    controller_base_path: '/api',
+    base_path: 'http://localhost:3000',
+    api_file_path: '/api',
+    controller_base_path: '/',
     api_file_path: 'public',
     base_api_controllers: [ApiController],
     parent_controller: ActionController::API
